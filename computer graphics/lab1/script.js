44dfc7d3837d0a0fdf9bac8113214a4e85db433e
MightyXi <100283671+MightyXi@users.noreply.github.com>
2023-10-20 01:38:26 +0300

    Update script.js

--- computer graphics/lab1/script.js
+++ computer graphics/lab1/script.js
@@ -5,14 +5,15 @@ var rgb_in = document.getElementsByClassName("rgb-in");
 var hsv_slider = document.getElementsByClassName("hsv-slider");
 var hsv_in = document.getElementsByClassName("hsv-in");
 var chooser = document.getElementById("chooser");
-var c = document.getElementById("canvas");
-var ctx = c.getContext("2d");
-update(0,0,0);
-function update(r,g,b){
-    document.body.style.backgroundColor = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+
+
+changeCanvas(0,0,0);
+function changeCanvas(r,g,b){
+    document.body.style.backgroundColor = 'rgb(' + r + ', ' + g + ', ' + b + ')';  
     var text = document.getElementById("text"); 
-    text.style.color = 'rgb(' + (255-r) + ', ' + (255-g) + ', ' + (255-b) + ')';   
+    text.style.color = 'rgb(' + (255-r) + ', ' + (255-g) + ', ' + (255-b) + ')'; 
 }
+
 
 const hexToRgb = (hex) => {
     const r = parseInt(hex.slice(1, 3), 16);
@@ -24,8 +25,130 @@ const hexToRgb = (hex) => {
 function chooseColor() {
     setRgb.apply(this, hexToRgb(chooser.value));
     rgbSlider();
-
 }
+
+
+
+
+function changeFromRgb(r,g,b){
+    setRgb(r,g,b);
+    setCmyk.apply(this, rgbToCmyk(r,g,b));
+    setHsv.apply(this, rgbToHsv(r,g,b));
+}
+
+function changeFromHsv(h,s,v){
+    setHsv(h,s,v);
+    setCmyk.apply(this, hsvToCmyk(h,s,v));
+    setRgb.apply(this, hsvToRgb(h,s,v));
+}
+
+function changeFromCmyk(){
+    setCmyk.apply(this, getCMYK());
+    setRgb.apply(this, cmykToRgb());
+    setHsv.apply(this, cmykToHsv());
+}
+
+function cmykToRgb(){
+    var cmyk = getCMYK();
+    var r = 255 * (1 - cmyk[0]) * (1 - cmyk[3]);
+    var g = 255 * (1 - cmyk[1]) * (1 - cmyk[3]);
+    var b = 255 * (1 - cmyk[2]) * (1 - cmyk[3]);
+    return [Math.round(r),Math.round(g),Math.round(b)];
+}
+
+function rgbToHsv(r, g, b) {
+    r /= 255;
+  g /= 255;
+  b /= 255;
+
+  let cmax = Math.max(r, g, b);
+  let cmin = Math.min(r, g, b);
+  let delta = cmax - cmin;
+  let h = 0;
+  let s = 0;
+  let l = 0;
+
+  if (delta === 0) {
+    h = 0;
+  } else if (cmax === r) {
+    h = ((g - b) / delta) % 6;
+  } else if (cmax === g) {
+    h = (b - r) / delta + 2;
+  } else {
+    h = (r - g) / delta + 4;
+  }
+
+  h = Math.round(h * 60);
+
+  if (h < 0) {
+    h += 360;
+  }
+
+  l = (cmax + cmin) / 2;
+
+  if (delta === 0) {
+    s = 0;
+  } else {
+    s = delta / (1 - Math.abs(2 * l - 1));
+  }
+
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return [h, s,l]
+  }
+  
+  function hsvToRgb(h, s, l) {
+   h /= 360;
+  s /= 100;
+  l /= 100;
+
+  let r, g, b;
+
+  if (s === 0) {
+    r = g = b = l;
+  } else {
+    const hue2rgb = (p, q, t) => {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    };
+
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+
+  const round = (num) => Math.round(num * 255);
+
+  return [round(r),round(g), round(b)];
+}
+function cmykToHsv(){
+    var rgb = cmykToRgb();
+    var hsv = rgbToHsv(rgb[0], rgb[1], rgb[2]);
+    return hsv;
+}
+
+function hsvToCmyk(h,s,v){
+    var rgb = hsvToRgb(h,s,v);
+    var cmyk = rgbToCmyk(rgb[0], rgb[1], rgb[2]);
+    return cmyk;
+}
+
+function rgbToCmyk(r,g,b){
+    var k = Math.min(1 - r/255, 1 - g/255, 1 - b/255);
+    var c = (1 - r/255 - k)/(1 - k);
+    var m = (1 - g/255 - k)/(1 - k);
+    var y = (1 - b/255 - k)/(1 - k);
+    return [c.toFixed(2),m.toFixed(2),y.toFixed(2),k.toFixed(2)];
+}
+
 
 function setCmyk(c,m,y,k){
     cmyk_slider[0].value = c;
@@ -100,131 +223,34 @@ function getHSVin(){
     return [h,s,v];
 }
 
-
-function changeFromRgb(r,g,b){
-    setRgb(r,g,b);
-    setCmyk.apply(this, rgbToCmyk(r,g,b));
-    setHsv.apply(this, rgbToHsv(r,g,b));
-}
-
-function changeFromHsv(h,s,v){
-    setHsv(h,s,v);
-    setCmyk.apply(this, hsvToCmyk(h,s,v));
-    setRgb.apply(this, hsvToRgb(h,s,v));
-}
-
-function changeFromCmyk(){
-    setCmyk.apply(this, getCMYK());
-    setRgb.apply(this, cmykToRgb());
-    setHsv.apply(this, cmykToHsv());
-}
-
-function cmykToRgb(){
-    var cmyk = getCMYK();
-    var r = 255 * (1 - cmyk[0]) * (1 - cmyk[3]);
-    var g = 255 * (1 - cmyk[1]) * (1 - cmyk[3]);
-    var b = 255 * (1 - cmyk[2]) * (1 - cmyk[3]);
-    return [Math.round(r),Math.round(g),Math.round(b)];
-}
-
-function rgbToHsv(r, g, b) {
-    r /= 255, g /= 255, b /= 255;
-  
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, v = max;
-  
-    var d = max - min;
-    s = max == 0 ? 0 : d / max;
-  
-    if (max == min) {
-      h = 0;
-    } else {
-      switch (max) {
-        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-        case g: h = (b - r) / d + 2; break;
-        case b: h = (r - g) / d + 4; break;
-      }
-  
-      h /= 6;
-    }
-  
-    return [Math.round(h*360), s.toFixed(2), v.toFixed(2)];
-  }
-  
-  function hsvToRgb(h, s, v) {
-    h = h/360;
-    var r, g, b;
-    var i = Math.floor(h * 6);
-    var f = h * 6 - i;
-    var p = v * (1 - s);
-    var q = v * (1 - f * s);
-    var t = v * (1 - (1 - f) * s);
-  
-    switch (i % 6) {
-      case 0: r = v, g = t, b = p; break;
-      case 1: r = q, g = v, b = p; break;
-      case 2: r = p, g = v, b = t; break;
-      case 3: r = p, g = q, b = v; break;
-      case 4: r = t, g = p, b = v; break;
-      case 5: r = v, g = p, b = q; break;
-    }
-  
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-  }
-function cmykToHsv(){
-    var rgb = cmykToRgb();
-    var hsv = rgbToHsv(rgb[0], rgb[1], rgb[2]);
-    return hsv;
-}
-
-function hsvToCmyk(h,s,v){
-    var rgb = hsvToRgb(h,s,v);
-    var cmyk = rgbToCmyk(rgb[0], rgb[1], rgb[2]);
-    return cmyk;
-}
-
-function rgbToCmyk(r,g,b){
-    var k = Math.min(1 - r/255, 1 - g/255, 1 - b/255);
-    var c = (1 - r/255 - k)/(1 - k);
-    var m = (1 - g/255 - k)/(1 - k);
-    var y = (1 - b/255 - k)/(1 - k);
-    return [c.toFixed(2),m.toFixed(2),y.toFixed(2),k.toFixed(2)];
-}
-
 function cmykSlider(){
     changeFromCmyk.apply(this, getCMYK());
-    update
-(rgb_slider[0].value, rgb_slider[1].value, rgb_slider[2].value);
+    changeCanvas(rgb_slider[0].value, rgb_slider[1].value, rgb_slider[2].value);
 }
 
 function cmykIn(){
     setCmyk.apply(this,getCMYKin());
     changeFromCmyk.apply(this,getCMYKin());
-    update
-(rgb_in[0].value, rgb_in[1].value, rgb_in[2].value);
+    changeCanvas(rgb_in[0].value, rgb_in[1].value, rgb_in[2].value);
 }
 
 function rgbSlider(){
     changeFromRgb.apply(this, getRGB());
-    update
-(rgb_slider[0].value, rgb_slider[1].value, rgb_slider[2].value);
+    changeCanvas(rgb_slider[0].value, rgb_slider[1].value, rgb_slider[2].value);
 }
 
 function rgbIn(){
     setRgb.apply(this,getRGBin());
     changeFromRgb.apply(this,getRGBin());
-    update
-(rgb_in[0].value, rgb_in[1].value, rgb_in[2].value);
+    changeCanvas(rgb_in[0].value, rgb_in[1].value, rgb_in[2].value);
 }
 
 function hsvIn(){
     setHsv.apply(this,getHSVin());
     changeFromHsv.apply(this,getHSVin());
-    update
-(rgb_in[0].value, rgb_in[1].value, rgb_in[2].value);
+    changeCanvas(rgb_in[0].value, rgb_in[1].value, rgb_in[2].value);
 }
 function hsvSlider(){
     changeFromHsv.apply(this, getHSV());
-    update
-(rgb_slider[0].value, rgb_slider[1].value, rgb_slider[2].value);
+    changeCanvas(rgb_slider[0].value, rgb_slider[1].value, rgb_slider[2].value);
 }
