e0086292872b35d1fc925095e3261e89b3579e07
MightyXi <100283671+MightyXi@users.noreply.github.com>
2023-11-22 19:51:20 +0300

    Update main.cpp

--- Numerical Methods/lab6/main.cpp
+++ Numerical Methods/lab6/main.cpp
@@ -8,7 +8,7 @@ int m = 14;
 int k = 2;
 int lit = 50;
 double eps = 0.0001;
-
+int kmain1, kmain2 ,kmain3;
 
 std::vector<double> generateVectorF(size_t size) {
     std::vector<double>  ans(size, 0);
@@ -222,6 +222,7 @@ std::vector<double> cg(std::vector<std::vector<double>> &A, std::vector<double> 
         }
         if (check(xl))
             break;
+        kmain1 = i+1;
     }
     return xl;
 }
@@ -291,16 +292,67 @@ std::vector<double> pcg(std::vector<std::vector<double>> &A, std::vector<double>
         }
         if (check(xl))
             break;
+        kmain2 = i+1;
     }
     return xl;
 }
 
+std::vector<double> pcg2(std::vector<std::vector<double>> &A, std::vector<double> &f, std::vector<double> &xZero,
+                        std::vector<std::vector<double>> M){
+    std::vector<double> r1, t1, t2;
+    std::vector<double> xl = xZero;
+    std::vector<double> fslae(size);
+    std::vector<double> mvxl = multiply(A, xl);
+    for (size_t i = 0; i < size; i++){
+        fslae[i] = f[i];
+        xl[i] = xZero[i];
+    }
+    std::vector<double> rl(size, 0);
+    std::vector<double> pl(size, 0);
+
+    for (size_t i = 0; i < size; i++){
+        rl[i] = fslae[i] - mvxl[i];
+        pl[i] = rl[i];
+    }
+
+    pl = precondition(rl, M);
+    t1 = precondition(rl, M);
+
+    double scal_rl_rl = scalMul(t1, rl);
+
+    for (size_t i = 0; i < lit; i++){
+        double rl_rl = scal_rl_rl;
+        std::vector<double> mvpl = multiply(A, pl);
+        double scal_mvpl_pl = scalMul(mvpl, pl);
+
+        double alpha = rl_rl/scal_mvpl_pl;
+        for (size_t j = 0; j < size; j++){
+            xl[j] += alpha*pl[j];
+            rl[j] -= alpha*mvpl[j];
+        }
+
+        t2 = precondition(rl, M);
+
+        scal_rl_rl = scalMul(t2, rl);
+        double beta = scal_rl_rl/rl_rl;
+
+        for (size_t j = 0; j < size; j++){
+            pl[j] = t2[j] +beta*pl[j];
+        }
+        if (check(xl))
+            break;
+        kmain3 = i+1;
+    }
+    return xl;
+}
 
 int main()
 {
     std::vector<std::vector<double>> M1(size, std::vector<double>(size, 0)),
             M2(size, std::vector<double>(size, 0));
     std::vector<std::vector<double>> matrix = generateMatrix(size);
+
+    std::vector<std::vector<double>> matrixClone = matrix;
     Minit(matrix, M1, M2);
     std::vector<double> x = generateVectorF(size);
     std::vector<double> f = multiply(matrix, x);
@@ -316,7 +368,7 @@ int main()
     const int time2 = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
     start = std::chrono::steady_clock::now();
-    std::vector<double> x3 = pcg(matrix, f, xZero, M2);
+    std::vector<double> x3 = pcg2(matrix, f, xZero, M2);
     end = std::chrono::steady_clock::now();
     const int time3 = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
@@ -326,36 +378,36 @@ int main()
     const int time4 = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
     std::cout << "cg" << "\n";
-
+    std::cout << "iternatin number " << kmain1 << std::endl;
     std::cout << "5 cordinats of solution ";
     print5(x_);
     std::cout << "otnositelnaya pogreshnost " << relativeError(x_, x) << "\n";
-    std::cout << "Norma vectora nevyazki " << vectorNevyazki(matrix, x_, f) << "\n";
+    std::cout << "Norma vectora nevyazki " << vectorNevyazki(matrixClone, x_, f) << "\n";
     std::cout << "Runtime " << time1 << " ms\n";
 
-    std::cout << "pcg with jakobi" << "\n";
-
+    std::cout << "pcg 1" << "\n";
+    std::cout << "iternatin number " << kmain2 << std::endl;
     std::cout << "5 cordinats of solution ";
     print5(x2);
     std::cout << "otnositelnaya pogreshnost " << relativeError(x2, x) << "\n";
-    std::cout << "Norma vectora nevyazki " << vectorNevyazki(matrix, x2, f) << "\n";
+    std::cout << "Norma vectora nevyazki " << vectorNevyazki(matrixClone, x2, f) << "\n";
     std::cout << "Runtime " << time2 << " ms\n";
 
 
-    std::cout << "pcg " << "\n";
+    std::cout << "pcg 2" << "\n";
+    std::cout << "iternatin number " << kmain3 << std::endl;
     std::cout << "5 cordinats of solution ";
     print5(x3);
     std::cout << "otnositelnaya pogreshnost " << relativeError(x3, x) << "\n";
-    std::cout << "Norma vectora nevyazki " << vectorNevyazki(matrix, x3, f) << "\n";
+    std::cout << "Norma vectora nevyazki " << vectorNevyazki(matrixClone, x3, f) << "\n";
     std::cout << "Runtime " << time3 << " ms\n";
 
     std::cout << "LDL\n";
-
     std::cout << "5 cordinats of solution ";
     print5(y);
 
     std::cout << "Otnositelnaya pogreshnost " << relativeError(y, x) << "\n";
-    std::cout << "Norma vectora nevyazki " << vectorNevyazki(matrix, y, f) << "\n";
+    std::cout << "Norma vectora nevyazki " << vectorNevyazki(matrixClone, y, f) << "\n";
     std::cout << "Runtime " << time4 << " ms\n";
     return 0;
 }
